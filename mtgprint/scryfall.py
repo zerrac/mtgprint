a5dfa8301239342c38ca412a8cb73e9dbb9ab3d5
ccarrez <c.carrez@ccarrez.webu.coop>
2022-11-14 11:35:09 +0100

    use card_api + randomize images

--- mtgprint/scryfall.py
+++ mtgprint/scryfall.py
@@ -2,9 +2,6 @@ import threading
 import time
 import requests
 from requests.adapters import HTTPAdapter, Retry
-import os.path
-import shutil
-import sys
 
 class Throttle:
 
@@ -51,73 +48,6 @@ def get_card_by_id(id):
         re  = http.get(SCRYFALL_URL+"/cards/%s" % id)
     re.raise_for_status()
     return re.json()
-
-def get_localized_card(set, collector_number, lang):
-    with rate_limiter:
-        re = http.get(SCRYFALL_URL+"/cards/%s/%s/%s" %(set,collector_number, lang))
-    re.raise_for_status()
-    return re.json()
-
-def search(payload, url=None):
-    with rate_limiter:
-        if url == None:
-            re = http.get(SCRYFALL_URL+"/cards/search", params=payload)
-        else:
-            re = http.get(url)
-    re.raise_for_status()
-    data = re.json()['data']
-
-    if re.json()['has_more']:
-        data = data + search(payload, re.json()['next_page'])
-
-    return data
-
-def get_prints(oracleid, order = "released", direction="desc", set_filter= None, number_filter=None):
-    query = "oracleid:%s" % oracleid
-    if set_filter != None:
-        query += " set:%s" % set_filter    
-        
-    if number_filter != None:
-        query += " number:%s" % number_filter    
-    
-    payload = {
-        'q': query,
-        'order': order,
-        'dir': direction,
-        'unique': 'prints'
-    }
-    return search(payload)
-
-def download(url, dest):
-    if not os.path.exists(dest):
-        try:
-            with rate_limiter:
-                re = http.get(url, stream=True)
-            re.raise_for_status()
-
-            with open(dest, 'xb') as f:
-                re.raw.decode_content = True
-                shutil.copyfileobj(re.raw, f)
-        except:
-            os.remove(dest)
-            raise
-    else:
-        print("Previous copy of the card found. Reusing it!")
-
-def image_getsize(url):
-    with rate_limiter:
-        re =  http.head(url)
-    re.raise_for_status()
-    if 'Content-Length' in re.headers.keys():
-        return int(re.headers['Content-Length'])
-    else: 
-        return 0
-
-def get_face_url(image):
-    if 'image_uris' in image:
-        return image['image_uris']['png']
-    elif 'card_faces' in image:
-        return  image['card_faces'][0]['image_uris']['png']
     
 def get_tokens(card):
     token_ids=list()
